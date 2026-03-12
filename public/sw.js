3a987b507485494f608c180c0d8fe231412c305d
edoardocanzi-ops <edoardo.canzi@gmail.com>
2026-03-12 23:13:53 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+// Questo file basta a soddisfare i requisiti Android per installare l'app
+self.addEventListener('fetch', function(event) {
+  // Lasciamo passare tutte le richieste normalmente
+});
